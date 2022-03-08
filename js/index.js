b23388477be0d2e10c46c97b82c1b9b3027892f4
mdsohag1 <abbasa90608@gmail.com>
2022-03-08 05:00:40 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+   var mixer = mixitup('.mixRow')
+});
+
+
+$(document).ready(function(){
+   $('#getting-started').countdown('2022/03/09', function(event){
+      $(this).html(event.strftime('<div><span>%D</span><span>days</span></div> <div><span>%H</span><span>Hr</span></div> <div><span>%M</span><span>Min</span></div> <div><span>%S</span><span>Sec</span></div>'));
+   });
+});
+
+
+
+$(document).ready(function(){
+   $('.owl-carousel').owlCarousel({
+      items: 5,
+      loop: true,
+      margin: 20,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      nav: true,
+      // responsiveClass: true,
+      // responsiveRefreshRate: true,
+      responsive:{
+         0:{items: 1},
+         768:{items: 1},
+         922:{items: 3},
+         1152:{items: 5},
+      }
+   });
+});
